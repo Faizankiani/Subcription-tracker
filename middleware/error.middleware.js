bbeecce8c3cb729939ed7eac552ed8b0504821e5
Faizan Kiani <faizankiani947@gmail.com>
2026-05-27 11:51:02 -0700

    Added error Middleware

---
+++ middleware/error.middleware.js
@@ -0,0 +1,22 @@
+const errorMiddleware = (err, req, res, next) => {
+
+    let stausCode = err.statusCode || 500;
+    let message = err.message || "Internal Server Error";
+
+    if (err.name === "CastError"){
+        message = " INvalid ID format";
+        statusCode = 400;
+    }
+    if (err.code === 11000){
+        message = "Duplicate key error";
+        statusCode = 400;
+    }
+    if (err.name === "ValidationError"){
+        message = Object.values(err.errors).map(val => val.message).join(', ');
+        statusCode = 404;
+    }
+    res.status(statusCode).json({
+        success: false,
+        message = message
+    });
+    
