60900b2366eb18e9cd73279d54a5dca985bcbc05
Faizan Kiani <faizankiani947@gmail.com>
2026-05-29 11:58:43 -0700

    Added user controller for Sign-In

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -50,3 +50,30 @@ const signUp = async (req, res, next) =>{
 
 
 }
+const signIn = async (req, res, next) =>{
+    try{
+        const {email, password} = req.body;
+        const user = await.User.findOne({email});
+        if (!user){
+            const error = new Error ("Invalid email or password");
+            error.statusCode = 401;
+            throw error;
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch){
+            const error = new Error ("Invalid email or password");
+            error.statusCode = 401;
+            throw error;
+        }   
+        const token = jwt.sign({id : user._id}, JWT_SECRET, {expiresIn: JWT_EXPIRES_IN});
+        res.status(200).json({
+            success: true,
+            message: "User successfully signed in",
+            token,
+        });
+    }
+    catch (error){
+        next(error);
+    }
+
+}
