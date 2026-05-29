c7d6893858b426ab6905546cfdfb4c588fb4a3be
Faizan Kiani <faizankiani947@gmail.com>
2026-05-29 11:53:13 -0700

    Added user controller for Sign-Up

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const app = express();
 import authRouter from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';   
 import connectToDb from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: false}));
 
 app.use('/auth', authRouter);
 app.use('/users', userRoutes);  
 
+app.use (errorMiddleware);
 app.get('/',(req,res)=>{
     res.send("Welcome to my API");
 });
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,11 @@
 import {Router} from 'express';
+import {signUp} from '../controller/auth.controller.js';
 const userRoutes = Router();
 
 userRoutes.get('/',(req,res)=>{
     res.send('Get all Users');
 });
+userRoutes.post('/signup', signUp);
 userRoutes.get('/:id',(req,res)=>{
     res.send(`Get user with id ${req.params.id}`);
 });
