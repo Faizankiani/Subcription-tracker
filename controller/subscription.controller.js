c454c9b8c92b01122b1bb95e5b7d20bacc2dfd5d
Faizan Kiani <faizankiani947@gmail.com>
2026-06-05 12:00:25 -0700

    Added get Subscription controller

--- controller/subscription.controller.js
+++ controller/subscription.controller.js
@@ -9,4 +9,18 @@ export const createSybscription = (req, res, next) =>{
     }catch(err){
         next(err);
     }
+};
+const getSubscription = async (req, res, next) =>{
+    try{
+        if (req.params.id !== req.user._id){
+            return res.status(403).json({message: "Forbidden"});
+        }
+        const subscription = await Subscription.find({user: req.params.id});
+        if (!subscription){
+            return res.status(404).json({message: "Subscription not found"});
+        }
+        res.json(subscription);
+    }catch(err){
+        next(err);
+    }
 }
